6a8b8cb44d5bbd12ea95dbd3db86d320b7161cef
yusuf9973 <139069015+yusuf9973@users.noreply.github.com>
2023-07-19 15:49:07 +0530

    main.py

--- main.py
+++ main.py
@@ -1,266 +1,145 @@
-import torch
+#torch implementation
 import torch.nn as nn
+import torch
+import torch.nn.functional as F
+class HalfNormal(object):
+    def __init__(self, scale, seed, device):
+        self.scale = scale
+        self.device = device
+        torch.manual_seed(seed)
+    def sample(self, sample_shape=torch.Size()):
+        result = torch.zeros(sample_shape).to(self.device)
+        result = result.normal_(mean=0, std=self.scale).abs()
+        return result
+class GFDConv(nn.Module):
+    def __init__(self, in_features,out_features, bias=False, scale=0.1, device='cpu', seed=307, fix_w=0, fix_h=0):
+        super(GFDConv, self).__init__()
+        self.conv = nn.Conv2d(9 * in_features, out_features, 1, bias=bias)
+        self.dis = HalfNormal(scale, seed, device)
+        self.size = None
+        self.device = device
+        self.direction_basis = torch.tensor([[-1, 1, -1, 1, -1, 1, 0, 0],
+                                             [-1, -1, 1, 1, 0, 0, -1, 1]]).float().view(-1).to(self.device)
+        if fix_w != 0 and fix_h != 0:
+            yy = torch.linspace(0, fix_h - 1, steps=fix_h).unsqueeze(1).repeat(1, fix_w).unsqueeze(-1).to(self.device)
+            xx = torch.linspace(0, fix_w - 1, steps=fix_w).unsqueeze(0).repeat(fix_h, 1).unsqueeze(-1).to(self.device)
+            self.base_coor = torch.cat([xx.repeat(1, 1, 8), yy.repeat(1, 1, 8)], dim=-1).to(self.device)
+            self.size = torch.tensor([fix_w] * 8 + [fix_h] * 8).float().to(self.device)
+    def forward(self, feat):
+        sample_coor = self.sample_process(feat.size(2), feat.size(3))
+        sample_coor_x = sample_coor[:, :, :8]
+        sample_coor_y = sample_coor[:, :, 8:]
+        feat = F.pad(feat, [1, 1, 1, 1]).to(sample_coor.device)
+        offset_feat = feat[:, :, sample_coor_y, sample_coor_x]
+        offset_feat = F.pad(offset_feat.permute(0, 4, 1, 2, 3).contiguous()
+                            .view(offset_feat.size(0), -1, offset_feat.size(2), offset_feat.size(3)), [1, 1, 1, 1])
+        feat = torch.cat([feat, offset_feat], dim=1)
+        feat = self.conv.to(feat.device)(feat)[:, :, 1:-1, 1:-1]
+        return feat
+    def sample_process(self, h, w):
+        if self.size is None:
+            yy = torch.linspace(0, h - 1, steps=h).unsqueeze(1).repeat(1, w).unsqueeze(-1).to(self.device)
+            xx = torch.linspace(0, w - 1, steps=w).unsqueeze(0).repeat(h, 1).unsqueeze(-1).to(self.device)
+            base_coor = torch.cat([xx.repeat(1, 1, 8), yy.repeat(1, 1, 8)], dim=-1).to(self.device)
+            size = torch.tensor([w] * 8 + [h] * 8).float().to(self.device)
+        else:
+            size = self.size
+            base_coor = self.base_coor
+        sample_ = self.dis.sample(torch.Size([h, w, 16]))
+        offset = sample_ * self.direction_basis * size
+        sample_coor = base_coor + offset
+        sample_coor[:, :, :8] = torch.clamp(sample_coor[:, :, :8], min=0, max=w - 1)
+        sample_coor[:, :, 8:] = torch.clamp(sample_coor[:, :, 8:], min=0, max=h - 1)
+        return (sample_coor + 1).long()
+def gaussian_dynamic_conv(in_channels, out_channels, kernel_size, padding, bias=False):
+    fix_w = 0
+    fix_h = 0
+    seed = 307
+    scale = 2
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    return GFDConv(in_channels, out_channels, bias, scale, device, seed, fix_w, fix_h)
 
-def conv2d(in_channels, out_channels, kernel_size, padding='same', stride=1, bias=False, kernel_initializer='he_normal'):
-  # calculate the padding size based on the kernel size and the 'same' option
-  if padding == 'same':
-    pad = (kernel_size - 1) // 2
-  else:
-    pad = padding
-  
-  # create a convolution layer with the given parameters
-  conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=pad, bias=bias)
-  
-  # initialize the weights of the convolution layer using He normal initialization
-  nn.init.kaiming_normal_(conv.weight, mode='fan_out', nonlinearity='relu')
-  
-  # return the convolution layer
-  return conv
+class conv_block_nested(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None,index=0):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.conv = nn.Sequential(
 
+            gaussian_dynamic_conv(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            gaussian_dynamic_conv(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )        
+    def forward(self, inputs):
+        return self.conv(inputs)
 
-def conv3x3(x, out_filters, strides=(1, 1)):
-    x = conv2d(x.shape[1], out_filters, 3, padding='same', stride=strides, bias=False)(x)
-    return x
-def basic_Block(input, out_filters, strides=(1, 1), with_conv_shortcut=False):
-    x = conv3x3(input, out_filters, strides)
-    x = torch.nn.BatchNorm2d(x.shape[1])(x)
-    x = torch.nn.ReLU()(x)
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=False,index = 0):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+        self.index = index
+        n1 = 64
+        filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.Up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv0_0 = conv_block_nested(n_channels, filters[0], filters[0],index)
+        self.conv1_0 = conv_block_nested(filters[0], filters[1], filters[1],index)
+        self.conv2_0 = conv_block_nested(filters[1], filters[2], filters[2],index)
+        self.conv3_0 = conv_block_nested(filters[2], filters[3], filters[3],index)
+        self.conv4_0 = conv_block_nested(filters[3], filters[4], filters[4],index)
+        self.conv0_1 = conv_block_nested(filters[0] + filters[1], filters[0], filters[0],index)
+        self.conv1_1 = conv_block_nested(filters[1] + filters[2], filters[1], filters[1],index)
+        self.conv2_1 = conv_block_nested(filters[2] + filters[3], filters[2], filters[2],index)
+        self.conv3_1 = conv_block_nested(filters[3] + filters[4], filters[3], filters[3],index)
+        self.conv0_2 = conv_block_nested(filters[0]*2 + filters[1], filters[0], filters[0],index)
+        self.conv1_2 = conv_block_nested(filters[1]*2 + filters[2], filters[1], filters[1],index)
+        self.conv2_2 = conv_block_nested(filters[2]*2 + filters[3], filters[2], filters[2],index)
+        self.conv0_3 = conv_block_nested(filters[0]*3 + filters[1], filters[0], filters[0],index)
+        self.conv1_3 = conv_block_nested(filters[1]*3 + filters[2], filters[1], filters[1],index)
+        self.conv0_4 = conv_block_nested(filters[0]*4 + filters[1], filters[0], filters[0],index)
+        self.final = nn.Conv2d(filters[0], n_classes, kernel_size=1)
+    def forward(self, x):
+        x0_0 = self.conv0_0(x)
+        x1_0 = self.conv1_0(self.pool(x0_0))
+        x0_1 = self.conv0_1(torch.cat([x0_0, self.Up(x1_0)], 1))
+        x2_0 = self.conv2_0(self.pool(x1_0))
+        x1_1 = self.conv1_1(torch.cat([x1_0, self.Up(x2_0)], 1))
+        x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.Up(x1_1)], 1))
+        x3_0 = self.conv3_0(self.pool(x2_0))
+        x2_1 = self.conv2_1(torch.cat([x2_0, self.Up(x3_0)], 1))
+        x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.Up(x2_1)], 1))
+        x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, self.Up(x1_2)], 1))
+        x4_0 = self.conv4_0(self.pool(x3_0))
+        x3_1 = self.conv3_1(torch.cat([x3_0, self.Up(x4_0)], 1))
+        x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.Up(x3_1)], 1))
+        x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.Up(x2_2)], 1))
+        x0_4 = self.conv0_4(torch.cat([x0_0, x0_1, x0_2, x0_3, self.Up(x1_3)], 1))
+        output = self.final(x0_4)
+        return output
+    def use_checkpointing(self):
+        self.conv0_0 = torch.utils.checkpoint(self.conv0_0)
+        self.conv1_0 = torch.utils.checkpoint(self.conv1_0)
+        self.conv0_1 = torch.utils.checkpoint(self.conv0_1)
+        self.conv2_0 = torch.utils.checkpoint(self.conv2_0)
+        self.conv1_1 = torch.utils.checkpoint(self.conv1_1)
+        self.conv0_2 = torch.utils.checkpoint(self.conv0_2)
+        self.conv3_0 = torch.utils.checkpoint(self.conv3_0)
+        self.conv2_1 = torch.utils.checkpoint(self.conv2_1)
+        self.conv1_2 = torch.utils.checkpoint(self.conv1_2)
+        self.conv0_3 = torch.utils.checkpoint(self.conv0_3)
+        self.conv4_0 = torch.utils.checkpoint(self.conv4_0)
+        self.conv3_1 = torch.utils.checkpoint(self.conv3_1)
+        self.conv2_2 = torch.utils.checkpoint(self.conv2_2)
+        self.conv1_3 = torch.utils.checkpoint(self.conv1_3)
+        self.conv0_4 = torch.utils.checkpoint(self.conv0_4)
+        self.final = torch.utils.checkpoint(self.final)
 
-    x = conv3x3(x, out_filters)
-    x = torch.nn.BatchNorm2d(x.shape[1])(x)
-
-    if with_conv_shortcut:
-        residual = torch.nn.Conv2d(input.shape[1], out_filters, 1, stride=strides, bias=False)(input)
-        residual = torch.nn.BatchNorm2d(residual.shape[1])(residual)
-        x = torch.add(x, residual)
-    else:
-        x = torch.add(x, input)
-
-    x = torch.nn.ReLU()(x)
-    return x
-def bottleneck_Block(input, out_filters, strides=(1, 1), with_conv_shortcut=False):
-    expansion = 4
-    de_filters = int(out_filters / expansion)
-
-    x = conv2d(input.shape[1], de_filters, 1, bias=False)(input)
-    x = torch.nn.BatchNorm2d(x.shape[1])(x)
-    x = torch.nn.ReLU()(x)
-
-    x = conv2d(x.shape[1], de_filters, 3, padding='same', stride=strides, bias=False)(x)
-    x = torch.nn.BatchNorm2d(x.shape[1])(x)
-    x = torch.nn.ReLU()(x)
-
-    x = conv2d(x.shape[1], out_filters, 1, bias=False)(x)
-    x = torch.nn.BatchNorm2d(x.shape[1])(x)
-
-    if with_conv_shortcut:
-        residual = conv2d(input.shape[1], out_filters, 1, stride=strides, bias=False)(input)
-        residual = torch.nn.BatchNorm2d(residual.shape[1])(residual)
-        x = torch.add(x, residual)
-    else:
-        x = torch.add(x, input)
-
-    x = torch.nn.ReLU()(x)
-    return x
-def stem_net(input):
-    x = conv2d(input.shape[1], 64, 3, padding='same', stride=(2, 2), bias=False)(input)
-    x = torch.nn.BatchNorm2d(x.shape[1])(x)
-    x = torch.nn.ReLU()(x)
-
-    x = conv2d(x.shape[1], 64, 3, padding='same', stride=(2, 2), bias=False)(x)
-    x = torch.nn.BatchNorm2d(x.shape[1])(x)
-    x = torch.nn.ReLU()(x)
-
-    x = bottleneck_Block(x, 256, with_conv_shortcut=True)
-    x = bottleneck_Block(x, 256, with_conv_shortcut=False)
-    x = bottleneck_Block(x, 256, with_conv_shortcut=False)
-    x = bottleneck_Block(x, 256, with_conv_shortcut=False)
-
-    return x
-def transition_layer1(x, out_filters_list=[32, 64]):
-    x0 = conv2d(x.shape[1], out_filters_list[0], 3, padding='same', bias=False)(x)
-    x0 = torch.nn.BatchNorm2d(x0.shape[1])(x0)
-    x0 = torch.nn.ReLU()(x0)
-
-    x1 = conv2d(x.shape[1], out_filters_list[1], 3, padding='same', stride=(2, 2), bias=False)(x)
-    x1 = torch.nn.BatchNorm2d(x1.shape[1])(x1)
-    x1 = torch.nn.ReLU()(x1)
-
-    return [x0, x1]
-def make_branch1_0(x, out_filters=32):
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    return x
-def make_branch1_1(x, out_filters=64):
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    return x
-def fuse_layer1(x):
-    x0_0 = x[0]
-    x0_1 = conv2d(x[1].shape[1], 32, 1, bias=False)(x[1])
-    x0_1 = torch.nn.BatchNorm2d(x0_1.shape[1])(x0_1)
-    x0_1 = torch.nn.Upsample(scale_factor=(2, 2))(x0_1)
-    x0 = torch.add(x0_0, x0_1)
-
-    x1_0 = conv2d(x[0].shape[1], 64, 3, padding='same', stride=(2, 2), bias=False)(x[0])
-    x1_0 = torch.nn.BatchNorm2d(x1_0.shape[1])(x1_0)
-    x1_1 = x[1]
-    x1 = torch.add(x1_0, x1_1)
-    return [x0, x1]
-def transition_layer2(x, out_filters_list=[32, 64, 128]):
-    x0 = conv2d(x[0].shape[1], out_filters_list[0], 3, padding='same', bias=False)(x[0])
-    x0 = torch.nn.BatchNorm2d(x0.shape[1])(x0)
-    x0 = torch.nn.ReLU()(x0)
-
-    x1 = conv2d(x[1].shape[1], out_filters_list[1], 3, padding='same', bias=False)(x[1])
-    x1 = torch.nn.BatchNorm2d(x1.shape[1])(x1)
-    x1 = torch.nn.ReLU()(x1)
-
-    x2 = conv2d(x[1].shape[1], out_filters_list[2], 3, padding='same', stride=(2, 2), bias=False)(x[1])
-    x2 = torch.nn.BatchNorm2d(x2.shape[1])(x2)
-    x2 = torch.nn.ReLU()(x2)
-
-    return [x0, x1, x2]
-def make_branch2_0(x, out_filters=32):
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    return x
-def make_branch2_1(x, out_filters=64):
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    return x
-def make_branch2_2(x, out_filters=128):
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    return x
-def fuse_layer2(x):
-    x0_0 = x[0]
-    x0_1 = conv2d(x[1].shape[1], 32, 1, bias=False)(x[1])
-    x0_1 = torch.nn.BatchNorm2d(x0_1.shape[1])(x0_1)
-    x0_1 = torch.nn.Upsample(scale_factor=(2, 2))(x0_1)
-    x0_2 = conv2d(x[2].shape[1], 32, 1, bias=False)(x[2])
-    x0_2 = torch.nn.BatchNorm2d(x0_2.shape[1])(x0_2)
-    x0_2 = torch.nn.Upsample(scale_factor=(4, 4))(x0_2)
-    x0 = torch.add(torch.add(x0_0, x0_1), x0_2)
-
-    x1_0 = conv2d(x[0].shape[1], 64, 3, padding='same', stride=(2, 2), bias=False)(x[0])
-    x1_0 = torch.nn.BatchNorm2d(x1_0.shape[1])(x1_0)
-    x1_1 = x[1]
-    x1_2 = conv2d(x[2].shape[1], 64, 1, bias=False)(x[2])
-    x1_2 = torch.nn.BatchNorm2d(x1_2.shape[1])(x1_2)
-    x1_2 = torch.nn.Upsample(scale_factor=(2, 2))(x1_2)
-    x1 = torch.add(torch.add(x1_0, x1_1), x1_2)
-
-    x2_0 = conv2d(x[0].shape[1], 32, 3, padding='same', stride=(2, 2), bias=False)(x[0])
-    x2_0 = torch.nn.BatchNorm2d(x2_0.shape[1])(x2_0)
-    x2_0 = torch.nn.ReLU()(x2_0)
-    x2_0 = conv2d(x2_0.shape[1], 128, 3, padding='same', stride=(2, 2), bias=False)(x2_0)
-    x2_0 = torch.nn.BatchNorm2d(x2_0.shape[1])(x2_0)
-    x2_1 = conv2d(x[1].shape[1], 128, 3, padding='same', stride=(2, 2), bias=False)(x[1])
-    x2_1 = torch.nn.BatchNorm2d(x2_1.shape[1])(x2_1)
-    x2_2 = x[2]
-    x2 = torch.add(torch.add(x2_0, x2_1), x2_2)
-
-    return [x0, x1, x2]
-def transition_layer3(x, out_filters_list=[32, 64, 128, 256]):
-    x0 = conv2d(x[0].shape[1], out_filters_list[0], 3, padding='same', bias=False)(x[0])
-    x0 = torch.nn.BatchNorm2d(x0.shape[1])(x0)
-    x0 = torch.nn.ReLU()(x0)
-
-    x1 = conv2d(x[1].shape[1], out_filters_list[1], 3, padding='same', bias=False)(x[1])
-    x1 = torch.nn.BatchNorm2d(x1.shape[1])(x1)
-    x1 = torch.nn.ReLU()(x1)
-
-    x2 = conv2d(x[2].shape[1], out_filters_list[2], 3, padding='same', bias=False)(x[2])
-    x2 = torch.nn.BatchNorm2d(x2.shape[1])(x2)
-    x2 = torch.nn.ReLU()(x2)
-
-    x3 = conv2d(x[2].shape[1], out_filters_list[3], 3, padding='same', stride=(2, 2), bias=False)(x[2])
-    x3 = torch.nn.BatchNorm2d(x3.shape[1])(x3)
-    x3 = torch.nn.ReLU()(x3)
-
-    return [x0, x1, x2, x3]
-def make_branch3_0(x, out_filters=32):
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    return x
-def make_branch3_1(x, out_filters=64):
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    return x
-def make_branch3_2(x, out_filters=128):
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    return x
-def make_branch3_3(x, out_filters=256):
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    x = basic_Block(x, out_filters, with_conv_shortcut=False)
-    return x
-
-def fuse_layer3(x):
-    x0_0 = x[0]
-    x0_1 = conv2d(32, 1,3, bias=False, kernel_initializer='he_normal')(x[1])
-    x0_1 = nn.BatchNorm2d(32)(x0_1)
-    x0_1 = nn.Upsample(scale_factor=2)(x0_1)
-    x0_2 = conv2d(32, 1,3, bias=False, kernel_initializer='he_normal')(x[2])
-    x0_2 = nn.BatchNorm2d(32)(x0_2)
-    x0_2 = nn.Upsample(scale_factor=4)(x0_2)
-    x0_3 = conv2d(32, 1,3, bias=False, kernel_initializer='he_normal')(x[3])
-    x0_3 = nn.BatchNorm2d(32)(x0_3)
-    x0_3 = nn.Upsample(scale_factor=8)(x0_3)
-    x0 = torch.cat([x0_0, x0_1, x0_2, x0_3], dim=-1)
-    return x0
-def final_layer(x, classes=1):
-    x = torch.nn.Upsample(scale_factor=(32, 32))(x)
-    x = conv2d(x.shape[1], classes, 1, bias=False)(x)
-    x = torch.nn.BatchNorm2d(x.shape[1])(x)
-    x = torch.nn.Sigmoid()(x)
-    x = torch.nn.functional.interpolate(x, size=(256, 256))
-    return x
-
-def seg_hrnet(inputs, classes):
-    x = stem_net(inputs)
-    x = transition_layer1(x)
-    x0 = make_branch1_0(x[0])
-    x1 = make_branch1_1(x[1])
-    x = fuse_layer1([x0, x1])
-    x = transition_layer2(x)
-    x0 = make_branch2_0(x[0])
-    x1 = make_branch2_1(x[1])
-    x2 = make_branch2_2(x[2])
-    x = fuse_layer2([x0, x1, x2])
-    x = transition_layer3(x)
-    x0 = make_branch3_0(x[0])
-    x1 = make_branch3_1(x[1])
-    x2 = make_branch3_2(x[2])
-    x3 = make_branch3_3(x[3])
-    x = fuse_layer3([x0, x1, x2, x3])
-
-    out = final_layer(x, classes=classes)
-
-    model = nn.Module(inputs=inputs, outputs=out)
-
-    return model
-
-
-input_tensor = torch.randn(1, 3, 256, 256)
-output_tensor = seg_hrnet(input_tensor,5)
-print(output_tensor)
+inp = torch.randn((1, 3, 256, 256))
+model = UNet(n_channels=3, n_classes=5, bilinear=False,index = 0)
+out = model(inp)
+print(out.shape)
